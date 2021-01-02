2e868d8d9008ef3c50507a5d7360079477bd0ae7
whiteononon <whiteon0618@gmail.com>
2021-01-02 23:46:55 +0900

    memo list screen

--- App.jsx
+++ App.jsx
@@ -1,25 +1,120 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import Hello from './src/components/Hello';
+import { StyleSheet, View, Text } from 'react-native';
 
 const App = () => (
   <View style={styles.container}>
-    <Hello bang={false}>World</Hello>
-    <Hello bang={false} style={{ fontSize: 10 }}>
-      World
-    </Hello>
-    {/* eslint-disable-next-line react/style-prop-object */}
-    <StatusBar style="auto" />
+    <View style={styles.appbar}>
+      <View style={styles.appbarInner}>
+        <Text style={styles.appbarTitle}>Memo App</Text>
+        <Text style={styles.appbarRight}>ログアウト</Text>
+      </View>
+    </View>
+
+    <View>
+      <View style={styles.memoListItem}>
+        <View>
+          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+          <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
+        </View>
+        <View style={styles.memoListItemDeleteButton}>
+          <Text>X</Text>
+        </View>
+      </View>
+
+      <View style={styles.memoListItem}>
+        <View>
+          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+          <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
+        </View>
+        <View style={styles.memoListItemDeleteButton}>
+          <Text>X</Text>
+        </View>
+      </View>
+
+      <View style={styles.memoListItem}>
+        <View>
+          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+          <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
+        </View>
+        <View style={styles.memoListItemDeleteButton}>
+          <Text>X</Text>
+        </View>
+      </View>
+    </View>
+
+    <View style={styles.circleButton}>
+      <Text style={styles.circleButtonLabel}>+</Text>
+    </View>
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f4f8',
+  },
+  appbar: {
+    width: '100%',
+    height: 104,
+    backgroundColor: '#467fd3',
+    justifyContent: 'flex-end',
+  },
+  appbarInner: {
     alignItems: 'center',
+  },
+  appbarRight: {
+    position: 'absolute',
+    right: 19,
+    bottom: 10,
+    color: 'rgba(255,255,255,0.8)',
+  },
+  appbarTitle: {
+    color: 'white',
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: 'bold',
+  },
+  memoListItem: {
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0,0,0,0.15)',
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484',
+  },
+  memoListItemDeleteButton: {
     justifyContent: 'center',
+  },
+  circleButton: {
+    backgroundColor: '#467fd3',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 8,
+  },
+  circleButtonLabel: {
+    flex: 1,
+    color: '#ffffff',
+    fontSize: 40,
   },
 });
 
